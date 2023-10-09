dbb203640360e385e507298fc2a8ac8bc94cc13f
jankendo <jankendo@icloud.com>
2023-10-09 21:37:47 +0900

    json出力をできるようになった。 実際のサイトに則ったテーブル設計に変更。

--- main.py
+++ main.py
@@ -1,61 +1,87 @@
 import os
-import csv
+import re
+import shutil
+import sqlite3
 import tkinter.filedialog as filedialog
 import zipfile
-import sqlite3
-import shutil
+import json
 
 
-def extract_fields(file_path, output_path):
-    # Zipファイルを解凍する
+def extract_fields(file_path, output_path, decknum):
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         zip_ref.extractall('output')
 
     db_path = 'output/collection.anki2'
-
-    # データベースに接続し、notesテーブルからflds列を取得する
     conn = sqlite3.connect(db_path)
     cursor = conn.execute('SELECT flds FROM notes')
 
-    # 出力ファイルを作成し、データをCSV形式で書き込む
-    with open(output_path, "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
+    deck = {
+        "id": str(decknum),
+        "jpnName": "英検デッキ",
+        "cards": []
+    }
+
+    cardnum = 0
+
+    with open(output_path, 'w') as file:
         for row in cursor:
             data = row[0]
             if data:
-                fields = data.split("\x1f")
-                writer.writerow(fields)
+                cardnum += 1
+                fild = data.split("\x1f")
+                jpn, eng = (fild[4], fild[5]) if detect_language(fild[4]) else (fild[5], fild[4])
 
-    # データベースの接続を閉じる
+                card = {
+                    "id": str(cardnum),
+                    "mp3Path": f"public/decks/deck1/audio/{cardnum}.mp3",
+                    "genre": "none",
+                    "word": fild[0],
+                    "wordtrn":fild[1],
+                    "sentence": eng,
+                    "blank": convert_to_blank_sentence(eng),
+                    "japanese": jpn,
+                    "difficulty": "980"
+                }
+                deck['cards'].append(card)
+
+        json.dump(deck, file, indent=4)
+
     conn.close()
     return output_path
 
 
+def convert_to_blank_sentence(sentence):
+    return "".join("_" if char.isalpha() else char for char in sentence)
+
+
+def detect_language(text):
+    japanese_range = (ord(u"ぁ"), ord(u"ゟ"))
+    return any(japanese_range[0] <= ord(char) <= japanese_range[1] for char in text)
+
+
 def main():
-    # フォルダの選択ダイアログを表示する
     root = filedialog.Tk()
     root.withdraw()
     folder_path = filedialog.askdirectory()
 
-    output_folder = "./csv"
+    output_folder = "./json"
     os.makedirs(output_folder, exist_ok=True)
+
+    decknum = 0
 
     for file_path in os.listdir(folder_path):
         if file_path.endswith(".apkg"):
-            # 初期化処理
             if os.path.exists('output'):
                 shutil.rmtree('output')
 
-            output_file = os.path.splitext(file_path)[0] + ".csv"
+            output_file = os.path.splitext(file_path)[0] + ".json"
             output_path = os.path.join(output_folder, output_file)
 
-            # 抽出・変換を実行する
-            extracted_path = extract_fields(os.path.join(folder_path, file_path), output_path)
+            extracted_path = extract_fields(os.path.join(folder_path, file_path), output_path, decknum)
+            decknum += 1
 
-            # 処理完了メッセージを表示する
             print(f"抽出・変換完了: {extracted_path}")
 
-            # 使用した一時フォルダを削除する
             shutil.rmtree('output')
 
 
